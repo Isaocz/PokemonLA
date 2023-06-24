ea70564be58b1880f9c0a22406d870ff1ec7f975
brokensc <965935569@qq.com>
2023-06-25 00:44:05 +0800

    2023/6/24

---
+++ Assets/Scripts/Skill/MagicalLeaf.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicalLeaf : Skill
+{
+    public float moveSpeed;//速度
+    public float rotateSpeed;//旋转速度
+    float timer;//计时器
+    GameObject enemy;
+    public float Waittime = 0.5f;//开始追踪时间
+    RaycastHit hit;
+
+    private float angle_fix = 10;//每次修正的角度
+    private float angle_differ = 10;//允许的差异角度
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartExistenceTimer();
+        timer += Time.deltaTime;
+        transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        if (enemy != null)
+        {
+            //对需要追踪物体和自身间的角度求解运算
+            Vector2 row = (enemy.transform.position - transform.position).normalized;
+            //获取两物体间夹角
+            float angle1 = Vector3.SignedAngle(Vector3.up, row, Vector3.forward);
+            //将夹角坐标和世界坐标的取值和范围对齐
+            angle1 = (angle1 + 270) % 360;
+            //获取弹幕自身世界坐标
+            float angle2 = transform.eulerAngles.z % 360;
+            //获取两个角度间的差异值并标准化
+            float angle3 = ((angle1 - angle2) + 360) % 360;
+            //对物体的角度做修正，使得物体x轴指向需要追踪的目标
+            //朝向需要追踪对象的方向调整角度，按照设定的值进行调整
+            if (angle3 < 180 - angle_differ)
+            {
+                Quaternion reAngle = Quaternion.Euler(0, 0, transform.eulerAngles.z - angle_fix);
+                transform.rotation = reAngle;
+            }
+            else if (angle3 > 180 + angle_differ)
+            {
+                Quaternion reAngle = Quaternion.Euler(0, 0, transform.eulerAngles.z + angle_fix);
+                transform.rotation = reAngle;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Empty enemy = other.gameObject.GetComponent<Empty>();
+        if (enemy != null)
+        {
+            //指定敌人目标
+            this.enemy = other.gameObject;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Empty target = collision.gameObject.GetComponent<Empty>();
+        if (target.tag == "Empty")
+        {
+            HitAndKo(target);
+            Destroy(gameObject);
+        }
+    }
+}
