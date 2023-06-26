72cc366b02b49c9d57b935279d838fbfa292d9bb
brokensc <965935569@qq.com>
2023-06-26 21:10:25 +0800

    2023/6/26 追踪、魔法叶

--- Assets/Scripts/Skill/Rotate.cs
+++ Assets/Scripts/Skill/Rotate.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
-using Unity.Mathematics;
 using UnityEngine;
 
-public class MagicalLeaf : Skill
+public class Rotate : MonoBehaviour
 {
+    //旋转角
+    float zAngle = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
-        //获取粒子效果，调整旋转方向
-        transform.GetChild(0).rotation = quaternion.Euler(-90, 0, 0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartExistenceTimer();
+        //刚体旋转，速度0.1f
+        transform.RotateAround(transform.position, Vector3.forward, zAngle);
     }
 }
