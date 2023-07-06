b41f5319c7f4e7a66f2368363a33e3f89505ec74
fufeng <fufeng@badambiz.com>
2023-07-06 22:08:13 +0800

    CameraAdapt

--- Assets/Scripts/Static/ConstantRoom.cs
+++ Assets/Scripts/Static/ConstantRoom.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ConstantRoom
 {
+    public static int ROOM_WIDTH = 30;
+    public static int ROOM_HIGHT = 24;
+
     public static int ROOM_INNER_WIDTH = 24;
     public static int ROOM_INNER_HIGHT = 14;
 }
---
+++ Assets/Scripts/UI/CameraAdapt.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdapt : MonoBehaviour
+{
+
+    public float halfHeightPre = 10.7415f;
+    float aimRatio;
+    float halfWidth;
+
+    private float lastwidth = 0f;
+    private float lastheight = 0f;
+
+    private void OnGUI()
+    {
+        if (lastwidth != Screen.width || lastheight != Screen.height)
+        {
+            lastwidth = Screen.width;
+            lastheight = Screen.height;
+            print("Resolution :" + Screen.width + " X" + Screen.height);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aimRatio = (float)ConstantRoom.ROOM_WIDTH / ConstantRoom.ROOM_HIGHT;
+        halfWidth = aimRatio * halfHeightPre;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        adapt();
+    }
+
+    void adapt()
+    {
+        float winRatio = (float)Screen.width / Screen.height;
+        if (winRatio >= aimRatio)
+        {
+            GetComponent<Camera>().orthographicSize = halfHeightPre;
+        }
+        else
+        {
+            GetComponent<Camera>().orthographicSize = halfWidth / winRatio;
+        }
+    }
+}
