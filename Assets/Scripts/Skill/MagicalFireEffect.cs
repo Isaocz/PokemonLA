a40dad2a73671417394add90d09cbb245c0b2e50
isaocho9988@gmail.com <isaocho9988@gmail.com>
2023-08-06 16:27:40 +0900

    2023/8/6/16:27 By:Isao 修复2.0bug 制作地鼠和爱管侍（异常状态对应尚未完成） 添加测试用技能 修改魔法火焰

--- Assets/Scripts/Skill/MagicalFireEffect.cs
+++ Assets/Scripts/Skill/MagicalFireEffect.cs
@@ -18,12 +18,23 @@ public class MagicalFireEffect : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //当敌人进入触发器时对敌人造成伤害并消失
-        if(collision.tag == "Empty")
+        if(collision.tag == "Empty" || collision.tag == "Room" || collision.tag == "Enviroment")
         {
-            Empty target = collision.GetComponent<Empty>();
-            transform.parent.GetComponent<MagicalFire>().HitAndKo(target);
-            target.LevelChange(-1, "SpA");
             Destroy(this.gameObject);
+            if (collision.tag == "Empty") {
+                Empty target = collision.GetComponent<Empty>();
+                transform.parent.GetComponent<MagicalFire>().HitAndKo(target);
+                target.LevelChange(-1, "SpA");
+            }
         }
+    }
+
+    private void OnDestroy()
+    {
+        var main = transform.GetChild(0).GetComponent<ParticleSystem>().main;
+        main.loop = false;
+        transform.GetChild(0).localScale = new Vector3(1, 1, 1);
+        transform.GetChild(0).rotation = Quaternion.Euler(0, 0, 0);
+        transform.DetachChildren();
     }
 }
