ec2b61d313857777c7942bab84200d330c492022
brokensc <965935569@qq.com>
2023-08-06 13:38:45 +0800

    魔法火焰、Konami Code

---
+++ Assets/Scripts/Skill/MagicalFire.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicalFire : Skill
+{
+    public float moveSpeed = 5f; // 移动速度
+    public float rotationSpeed = 50f; // 旋转速度
+
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveDirection = transform.right;
+    }
+
+    void Update()
+    {
+        StartExistenceTimer();
+        // 移动对象
+        rb.velocity = moveDirection * moveSpeed;
+
+        // 绕自身几何中心旋转
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
+
