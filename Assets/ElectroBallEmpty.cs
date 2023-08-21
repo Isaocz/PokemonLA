d678422f54c69d3155d8c61ccb6b47adb9107ac2
brokensc <965935569@qq.com>
2023-08-22 00:07:22 +0800

    摄像修改、梦幻电球、冰冻光束

---
+++ Assets/ElectroBallEmpty.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ElectroBallEmpty : Projectile
+{
+    Vector3 mewposition;
+    float speed = 1f;
+    float rotationspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 20f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 绕 Mew 旋转
+        transform.RotateAround(mewposition, Vector3.forward, rotationspeed * Time.deltaTime);
+
+        // 向外移动
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerControler playerControler = collision.GetComponent<PlayerControler>();
+            Pokemon.PokemonHpChange(empty.gameObject, playerControler.gameObject, 0, SpDmage, 0, Type.TypeEnum.Fire);
+            if (playerControler != null)
+            {
+                playerControler.KnockOutPoint = 2.5f;
+                playerControler.KnockOutDirection = (playerControler.transform.position - transform.position).normalized;
+            }
+        }
+    }
+    public void Initialize(Vector3 mewPosition, float rotationSpeed)
+    {
+        mewposition = mewPosition;
+        rotationspeed = rotationSpeed;
+    }
+}
