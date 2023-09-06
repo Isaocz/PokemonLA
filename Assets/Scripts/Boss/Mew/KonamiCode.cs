4fa0dd51c5134424bea168383e5709499d387614
brokensc <965935569@qq.com>
2023-09-06 23:42:31 +0800

    完成梦幻，玩家技能叶刃重做

--- Assets/Scripts/Boss/Mew/KonamiCode.cs
+++ Assets/Scripts/Boss/Mew/KonamiCode.cs
@@ -7,7 +7,6 @@ public class KonamiCode : MonoBehaviour
     private KeyCode[] konamiCode = { KeyCode.UpArrow, KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.B, KeyCode.A, KeyCode.B, KeyCode.A };
     private int currentIndex = 0;
     public GameObject MewBossPrefab;
-    public GameObject MewRoom;
 
     void Update()
     {
@@ -34,18 +33,8 @@ public class KonamiCode : MonoBehaviour
     {
         // 在这里编写触发效果的代码
         Debug.Log("Konami Code triggered!");
-        GameObject mewPrefab = Resources.Load<GameObject>("Mew"); // 加载Mew预制件
-        if (mewPrefab != null)
-        {
-            GameObject MewBoss = Instantiate(mewPrefab, transform.position, Quaternion.identity);
-            MewRoom.transform.Find("Empty").gameObject.SetActive(true);
-            MewBoss.transform.SetParent(MewRoom.transform.Find("Empty"));
-            Destroy(gameObject); // 销毁当前的MewNPC对象
-        }
-        else
-        {
-            Debug.LogError("Failed to load Mew!");
-        }
+        GameObject MewBoss = Instantiate(MewBossPrefab, transform.position, Quaternion.identity, transform.parent);
+        Destroy(gameObject); // 销毁当前的MewNPC对象
     }
 }
 
