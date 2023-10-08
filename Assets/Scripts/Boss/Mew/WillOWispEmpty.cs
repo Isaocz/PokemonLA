36d25bbafc5edd131685715e3a4dc4cd74d6e7ed
brokensc <965935569@qq.com>
2023-10-08 19:41:16 +0800

    大风车

--- Assets/Scripts/Boss/Mew/WillOWispEmpty.cs
+++ Assets/Scripts/Boss/Mew/WillOWispEmpty.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WillOWispEmpty : MonoBehaviour
 {
-    public float moveSpeed;
+    private float moveSpeed;
     private Vector3 moveDirection;
     public GameObject mew;
 
