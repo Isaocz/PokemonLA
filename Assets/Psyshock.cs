b91805815a88171840a99e48c45b90c1ad9818b0
brokensc <965935569@qq.com>
2023-11-08 22:22:27 +0800

    精神强念

--- Assets/Psyshock.cs
+++ Assets/Psyshock.cs
@@ -30,6 +30,9 @@ public class Psyshock : Skill
             Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 10f, LayerMask.GetMask("Empty", "EmptyFly"));
             float nearestDistance = Mathf.Infinity;
             Transform nearestEnemy = null;
+            Vector3 hitPosition = Vector3.zero;
+            List<Transform> Targets = new List<Transform>();
+
             foreach (Collider2D collider in colliders)
             {
                 Vector2 direction = (collider.transform.position - transform.position).normalized;
@@ -37,26 +40,67 @@ public class Psyshock : Skill
                 if (angle < 30f) // 扇形范围为60度
                 {
                     float distance = Vector3.Distance(transform.position, collider.transform.position);
-                    if (distance < nearestDistance)
+                    RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, distance, LayerMask.GetMask("Enviroment", "Room"));
+                    if (!hit)
                     {
-                        nearestDistance = distance;
-                        nearestEnemy = collider.transform;
+                        Targets.Add(collider.transform);//把扇形范围内的敌人放进Targets里
                     }
                 }
             }
 
-            if (nearestEnemy != null)
+            if (Targets.Count > 0) // 如果存在合适的敌人
             {
-                Empty target = nearestEnemy.GetComponent<Empty>();
-                HitAndKo(target);
-                lineRenderer.SetPosition(0, transform.position);
-                lineRenderer.SetPosition(1, target.transform.position);
-                summonPoint = target.transform.position;
+                foreach (Transform target in Targets)
+                {
+                    float distance = Vector3.Distance(transform.position, target.transform.position);
+                    if (distance < nearestDistance)
+                    {
+                        nearestDistance = distance;
+                        nearestEnemy = target;
+                    }
+                }
+                if (nearestEnemy != null)
+                {
+                    Empty target = nearestEnemy.GetComponent<Empty>();
+                    HitAndKo(target);
+                    lineRenderer.SetPosition(0, transform.position);
+                    lineRenderer.SetPosition(1, target.transform.position);
+                    summonPoint = target.transform.position;
+                }
             }
             else
-            {
-                Skill0();
+            {//扇形范围内没有合适的敌怪，选择扇形范围内最近的敌怪，并将开始点和结束点分别定义
+                foreach (Collider2D collider in colliders)
+                {
+                    Vector2 direction = (collider.transform.position - transform.position).normalized;
+                    float angle = Vector2.Angle(transform.right, direction);
+                    if (angle < 30f)
+                    {
+                        float distance = Vector3.Distance(transform.position, collider.transform.position);
+                        if (distance < nearestDistance)
+                        {
+                            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, distance, LayerMask.GetMask("Enviroment", "Room"));
+                            
+                            nearestDistance = distance;
+                            nearestEnemy = collider.transform;
+                            hitPosition = hit.point;
+                        }
+                    }
+                }
+                if (nearestEnemy != null)//甚至扇形范围内没有敌怪
+                {
+                    lineRenderer.SetPosition(0, transform.position);
+                    lineRenderer.SetPosition(1, hitPosition);
+                    summonPoint = hitPosition;
+                }
+                else
+                {
+                    lineRenderer.SetPosition(0, transform.position);
+                    lineRenderer.SetPosition(1, transform.position + transform.right * 10);
+                    summonPoint = transform.position + transform.right * 10;
+                }
             }
+            
         }
         if (SkillFrom != 2)
         {
@@ -72,7 +116,7 @@ public class Psyshock : Skill
 
     void Skill0()
     {//激光效果
-        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.right, 8, LayerMask.GetMask("Empty", "EmptyFly", "Enviroment", "Room"));
+        RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.right, 10, LayerMask.GetMask("Empty", "EmptyFly", "Enviroment", "Room"));
         Vector2 EndPoint = hitinfo.point;
         //如果击中敌方宝可梦，则造成伤害
         if (hitinfo)
@@ -91,8 +135,8 @@ public class Psyshock : Skill
         {
             //否则正常显示
             lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, transform.position + transform.right * 8);
-            summonPoint = transform.position + transform.right * 8;
+            lineRenderer.SetPosition(1, transform.position + transform.right * 10);
+            summonPoint = transform.position + transform.right * 10;
         }
     }
 
