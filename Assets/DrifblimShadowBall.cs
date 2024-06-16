8808e4b8e584df654b5f66493d68eda560d9b9db
brokensc <965935569@qq.com>
2024-06-16 17:04:00 +0800

    随风球

---
+++ Assets/DrifblimShadowBall.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrifblimShadowBall : Projectile
+{
+    public float moveSpeed = 5f;
+    public Vector2 direction;
+    public float MaxRange = 20f; // ×î´óÒÆ¶¯¾àÀë
+    private Animator animator;
+    bool isCanNotMove;
+    Vector3 StartPostion;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isCanNotMove = false;
+        StartPostion = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (!isCanNotMove)
+        {
+            Vector3 postion = transform.position;
+            postion.x += direction.x * moveSpeed * Time.deltaTime;
+            postion.y += direction.y * moveSpeed * Time.deltaTime;
+            transform.position = postion;
+            if ((StartPostion - transform.position).magnitude > MaxRange)
+            {
+                BallBreak();
+            }
+        }
+    }
+
+    void BallBreak()
+    {
+        if (!isCanNotMove)
+        {
+            transform.GetComponent<Collider2D>().enabled = false;
+            animator.SetTrigger("Over");
+            isCanNotMove = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player"||collision.tag == "Room" || collision.tag == "Enviroment"|| (empty.isEmptyInfatuationDone && collision.tag == "Empty"))
+        {
+            BallBreak();
+            if (collision.tag == "Player")
+            {
+                PlayerControler playerControler = collision.GetComponent<PlayerControler>();
+                Pokemon.PokemonHpChange(empty.gameObject, collision.gameObject, 0, 80, 0, Type.TypeEnum.Ghost);
+                if(playerControler != null)
+                {
+                    playerControler.KnockOutPoint = 5;
+                    playerControler.KnockOutDirection = (playerControler.transform.position - transform.position).normalized;
+                }
+            }
+            else if (empty.isEmptyInfatuationDone && collision.tag == "Empty")
+            {
+                Empty e = collision.GetComponent<Empty>();
+                Pokemon.PokemonHpChange(empty.gameObject, e.gameObject, 0, 80, 0, Type.TypeEnum.Ghost);
+            }
+        }
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
