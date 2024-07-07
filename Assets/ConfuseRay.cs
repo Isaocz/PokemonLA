2e78c3bee6742eaed521a7b8aa7ad64c07893823
brokensc <965935569@qq.com>
2024-07-07 11:15:23 +0800

    2024/7/7 3技能、随风球bug

---
+++ Assets/ConfuseRay.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfuseRay : Skill
+{
+    public float moveSpeed = 5f;
+    public GameObject confusionEffect;
+    void Update()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        StartExistenceTimer();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Empty")
+        {
+            Empty target = collision.GetComponent<Empty>();
+            if (target != null)
+            {
+                target.ConfusionFloatPlus(1);
+                Destroy(gameObject);
+                GameObject effect = Instantiate(confusionEffect, target.transform.position + new Vector3(0,1,2f), Quaternion.identity);
+                effect.GetComponent<ConfuseRayEffect>().target = target.transform;
+                Destroy(effect, 1f);
+            }
+        }
+    }
+}
