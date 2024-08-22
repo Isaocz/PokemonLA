7d378b5eb358830b19fc763dc0c85255bae3363d
brokensc <965935569@qq.com>
2024-08-22 13:50:30 +0800

    小修改 8/22

--- Assets/Scripts/Boss/Mew/TeraBlastEmpty.cs
+++ Assets/Scripts/Boss/Mew/TeraBlastEmpty.cs
@@ -3,24 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 public class TeraBlastEmpty : Projectile
 {
-    public float laserDuration = 1.0f;
-    public Color initialColor = Color.yellow;
-    public Color finalColor = Color.red;
+    public float laserDuration = 0.5f;
     float angle;//接收传入的角度
-    public float rotationSpeed = 50f;
     public Vector3 startpoint;
     public Vector3 endpoint;
     Mew mew;
 
     Vector3 direction;
-    bool isSafe = true;
-
     private LineRenderer lineRenderer;
-    private int shootingTime;
+    private float timer;
 
     //获取起始特效
     GameObject StartVFX;
-    //获取末尾特效
     GameObject EndVFX;
 
     private void Awake()
@@ -30,61 +24,44 @@ public class TeraBlastEmpty : Projectile
     }
     private void Start()
     {
-        shootingTime = 1;
-        // 获取从起始点指向终点的标准向量
         direction = (endpoint - startpoint).normalized;
-        isSafe = true;
-
-        lineRenderer.startColor = initialColor;
-        lineRenderer.endColor = initialColor;
-        //获取组件
-        EndVFX = transform.Find("EndVFX").gameObject;
-        StartVFX = transform.Find("StartVFX").gameObject;
+        EndVFX = transform.GetChild(1).gameObject;
+        StartVFX = transform.GetChild(0).gameObject;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
         //限制射线出现的位置，防止出现在出生点
         lineRenderer.SetPosition(0, startpoint);
         lineRenderer.SetPosition(1, endpoint);
-        //修改激光颜色
-        Invoke("ChangeColor", laserDuration);
-        if (mew.LaserChange)
-        {
-            rotationSpeed = rotationSpeed * -1;
-        }
+
+        lineRenderer.enabled = false;
+        EndVFX.SetActive(false);
+        StartVFX.SetActive(true);
     }
     void Update()
     {
-        angle += Time.deltaTime * rotationSpeed;
-        endpoint = startpoint + new Vector3(40f * Mathf.Cos(Mathf.Deg2Rad * angle), 40f * Mathf.Sin(Mathf.Deg2Rad * angle), 0f);
-        direction = (endpoint - startpoint).normalized;
-        //修改射线的位置
-        lineRenderer.SetPosition(0, startpoint);
-        lineRenderer.SetPosition(1, endpoint);
-        rayPosition();
-    }
-    private void ChangeColor()
-    {
-        //变色
-        lineRenderer.startColor = finalColor;
-        lineRenderer.endColor= finalColor;
-        isSafe = false;
-        if (mew.currentPhase != 2||shootingTime==3)
-        Invoke("Delete", 2f);
-        else
+        //计时器工作
+        timer += Time.deltaTime;
+        if(timer < 1f)
         {
-            StartCoroutine(Continue());
-            IEnumerator Continue()
-            {
-                yield return new WaitForSeconds(2f);
-                if (shootingTime <= 3)
-                {
-                    shootingTime++;
-                    ContinueShooting();
-                }
-            }
+            lineRenderer.enabled = false;
+            EndVFX.SetActive(false);
+            StartVFX.SetActive(true);
         }
-    }
-    private void Delete()
-    {
-        Destroy(gameObject);
+        else if(timer >= 1f && timer < 1.4f)
+        {
+            lineRenderer.enabled = true;
+            EndVFX.SetActive(true);
+
+            endpoint = startpoint + new Vector3(40f * Mathf.Cos(Mathf.Deg2Rad * angle), 40f * Mathf.Sin(Mathf.Deg2Rad * angle), 0f);
+            direction = (endpoint - startpoint).normalized;
+            //修改射线的位置
+            lineRenderer.SetPosition(0, startpoint);
+            lineRenderer.SetPosition(1, endpoint);
+            rayPosition();
+        }
+        else if(timer >= 1.4f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void rayPosition()
@@ -100,22 +77,22 @@ public class TeraBlastEmpty : Projectile
         }
         if ((hitinfo) || (hitinfoTop) || (hitinfoBottom))
         {
-            EndPoint = hitinfo.point;
+            endpoint = hitinfo.point;
             RaycastHit2D EndRay = hitinfo;
             if ((hitinfoTop.point - hitinfo.point).magnitude >= 0.5f)
             {
-                EndRay = hitinfoTop; EndPoint = hitinfoTop.point;
+                EndRay = hitinfoTop; endpoint = hitinfoTop.point;
             }
             if ((hitinfoBottom.point - hitinfo.point).magnitude >= 0.5f)
             {
-                EndRay = hitinfoBottom; EndPoint = hitinfoBottom.point;
+                EndRay = hitinfoBottom; endpoint = hitinfoBottom.point;
 
             }
-            if (EndPoint == Vector2.zero) { EndPoint = hitinfo.point; }
+            if (EndPoint == Vector2.zero) { endpoint = hitinfo.point; }
 
 
             //如果击中玩家，则对玩家造成伤害
-            if (EndRay.collider != null && EndRay.collider.gameObject.tag == "Player" && isSafe == false)
+            if (EndRay.collider != null && EndRay.collider.gameObject.tag == "Player")
             {
                 PlayerControler playerControler = EndRay.collider.GetComponent<PlayerControler>();
                 Pokemon.PokemonHpChange(empty.gameObject, EndRay.collider.gameObject, 0, SpDmage, 0, Type.TypeEnum.Normal);
@@ -144,18 +121,5 @@ public class TeraBlastEmpty : Projectile
         endpoint = Endpoint;
         angle = Angle;
     }
-    public void SetColors(Color StartColor, Color EndColor)
-    {
-        //传入颜色
-        initialColor = StartColor;
-        finalColor = EndColor;
-    }
 
-    void ContinueShooting()
-    {
-        lineRenderer.startColor = initialColor;
-        lineRenderer.endColor = initialColor;
-        isSafe = true;
-        Invoke("ChangeColor", laserDuration);
-    }
 }
