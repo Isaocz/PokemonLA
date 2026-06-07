c9bf5b75099c81396bee1ed279bc897408866a0a
isaocho9988@gmail.com <isaocho9988@gmail.com>
2026-06-08 04:36:06 +0900

    2026/6/8/3：33 By:Isao  修改敌人 几何雪花

--- Assets/Scripts/Empty/NormalEmpty/Cryogonal/Cryogonal.cs
+++ Assets/Scripts/Empty/NormalEmpty/Cryogonal/Cryogonal.cs
@@ -34,6 +34,7 @@ public class Cryogonal : Empty
     float MoveTime = 1.0f;
 
 
+    float SpeedAlpha = 1.0f;//速度加成
 
     // Start is called before the first frame update
     void Start()
@@ -101,7 +102,7 @@ public class Cryogonal : Empty
 
                 StateTimer += Time.deltaTime;
                 //进入移动模式
-                if (StateTimer >= AtkCDTime && isInAttackState)
+                if (((!isFearDone && StateTimer >= AtkCDTime) || (isFearDone && StateTimer >= 0.7f)) && isInAttackState)
                 {
                     isInAttackState = false;
                     StateTimer = 0;
@@ -112,22 +113,32 @@ public class Cryogonal : Empty
                     }
                     else { TargetPosition = InfatuationForRangeRayCastEmpty(9).transform.position; }
                     Director = (new Vector2(((TargetPosition - (Vector2)transform.position).normalized.x > 0) ? 1 : -1, ((TargetPosition - (Vector2)transform.position).normalized.y > 0) ? 1 : -1));
+                    if (isFearDone) { Director = _mTool.TiltMainVector2(Quaternion.AngleAxis(Random.Range(0, 360), Vector3.forward) * Vector2.right); }
+                    if (isEmptyConfusionDone) { Director = ConfusionDir(Director, 40.0f); }
                     animator.SetFloat("LookX", Director.x);
                 }
                 //进入攻击模式
                 if (StateTimer >= MoveTime && !isInAttackState)
                 {
-                    AtkCount++;
-                    if (AtkCount > 3) { AtkCount = 0; }
-                    SetAtkCDTime();
-                    StateTimer = 0; isInAttackState = true;
+                    if (!isFearDone)
+                    {
+                        AtkCount++;
+                        if (AtkCount > 4)
+                        {
+                            AtkCount = 0;
+                        }
+                        SetAtkCDTime();
+                    }
+                    StateTimer = 0;
+                    isInAttackState = true;
                     if (!isFearDone) {
                         animator.SetTrigger("Atk"); }
                 }
                 if (!isInAttackState)
                 {
                     //Debug.Log(Time.deltaTime);
-                    rigidbody2D.MovePosition(new Vector2(transform.position.x + Director.x * speed * Time.deltaTime * 0.6f * (isFearDone ? -1.3f : 1), transform.position.y + Director.y * speed * Time.deltaTime * 0.6f * (isFearDone ? -1.3f : 1)));
+                    //Debug.Log(Director);
+                    rigidbody2D.MovePosition(new Vector2(transform.position.x + Director.x * Time.deltaTime * (isFearDone ? -5.2f : (speed * SpeedAlpha * 0.8f)), transform.position.y + Director.y * Time.deltaTime * (isFearDone ? -5.2f : (speed * SpeedAlpha * 0.8f))));
                     if (isFearDone)
                     {
                         animator.SetFloat("LookX", -Director.x);
@@ -139,6 +150,16 @@ public class Cryogonal : Empty
                 }
 
             }
+
+            if ((isEmptyFrozenDone || isSleepDone || isSilence) && isInAttackState)
+            {
+                isInAttackState = false;
+                StateTimer = 0;
+                animator.SetTrigger("Sleep");
+                animator.ResetTrigger("Atk");
+                AtkCount = 0;
+            }
+                
             animator.SetFloat("Speed", ((Vector2)transform.position - LastPosition).magnitude);
             LastPosition = transform.position;
         }
@@ -165,22 +186,27 @@ public class Cryogonal : Empty
             case 0:
                 AtkCDTime = 6.0f;
                 MoveTime = 1.0f;
-                speed = 3.8f;
+                SpeedAlpha = 1.0f;
                 break;
             case 1:
                 AtkCDTime = 2.6f;
                 MoveTime = 1.0f;
-                speed = 4.9f;
+                SpeedAlpha = 1.3f;
                 break;
             case 2:
                 AtkCDTime = 1.2f;
                 MoveTime = 0.7f;
-                speed = 6.0f;
+                SpeedAlpha = 1.6f;
                 break;
             case 3:
                 AtkCDTime = 0.8f;
                 MoveTime = 0.0f;
-                speed = 0;
+                SpeedAlpha = 0.0f;
+                break;
+            case 4:
+                AtkCDTime = 0.8f;
+                MoveTime = 0.0f;
+                SpeedAlpha = 0.0f;
                 break;
         }
     }
@@ -190,10 +216,12 @@ public class Cryogonal : Empty
         if (!isFearDone)
         {
             Debug.Log(isEmptyConfusionDone);
+            int c = (AtkCount == 0 ? 1 : AtkCount);
             if (!isEmptyConfusionDone) {
+                //Debug.Log(AtkCount);
                 for (int i = 0; i < 6; i++)
                 {
-                    CryogonalIceShard e1 = Instantiate(CIS, transform.position, Quaternion.Euler(0, 0, ((AtkCount % 2 == 0) ? 0 : 90) + i * 60));
+                    CryogonalIceShard e1 = Instantiate(CIS, transform.position, Quaternion.Euler(0, 0, ((c % 2 == 0) ? 0 : 90) + i * 60));
                     e1.empty = this;
                 } 
             }
@@ -201,11 +229,27 @@ public class Cryogonal : Empty
             {
                 for (int i = 0; i < 4; i++)
                 {
-                    CryogonalIceShard e1 = Instantiate(CIS, transform.position, Quaternion.Euler(0, 0, ((AtkCount % 2 == 0) ? 0 : 90) + i * 90));
+                    CryogonalIceShard e1 = Instantiate(CIS, transform.position, Quaternion.Euler(0, 0, ((c % 2 == 0) ? 0 : 45) + i * 90));
                     e1.empty = this;
                 }
             }
 
         }
+    }
+
+
+    /// <summary>
+    /// 混乱时角度偏转
+    /// </summary>
+    Vector2 ConfusionDir(Vector2 dir, float Alpha)
+    {
+        if (isEmptyConfusionDone)
+        {
+            Vector2 d = Quaternion.AngleAxis(Random.Range(-Alpha, Alpha), Vector3.forward) * dir;
+            Debug.Log(d);
+            return d;
+        }
+        //Debug.Log(dir);
+        return dir;
     }
 }
